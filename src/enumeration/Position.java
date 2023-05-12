032a6e1cc4e41361be93f68f3bb4e7750e4ae19e
FindlayRoyds <findlayroyds@gmail.com>
2023-05-12 15:16:51 +1200

    created position enum

---
+++ src/enumeration/Position.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package src.enumeration;
+
+/**
+ * @author Findlay Royds
+ * This enum defines all the positions available for athletes to play in a team
+ */
+public enum Position {
+	SHORT_SHOOTER,
+	LONG_SHOOTER,
+	DUNKER,
+	DRIBBLER,
+	DEFENDER
+}
