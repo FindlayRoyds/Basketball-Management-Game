908aabb7d96b55e5e58b578dd33a8d2a75318570
FindlayRoyds <findlayroyds@gmail.com>
2023-05-12 16:33:34 +1200

    created statistic enum

---
+++ src/enumeration/Statistic.java
@@ -0,0 +1,19 @@
+/**
+ * this enum defines the statistics of athletes
+ * 
+ * @author Findlay Royds
+ * @version 1.0, May 2023.
+ */
+package enumeration;
+
+public enum Statistic {
+	STAMINA,
+	FITNESS,
+	OFFENCE,
+	DEFENCE,
+	JUMPING,
+	DRIBBLING,
+	HEIGHT,
+	SHOOTING_ACCURACY,
+	SHOOTING_POWER
+}
