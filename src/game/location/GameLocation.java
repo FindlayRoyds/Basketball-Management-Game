3e2c1914f6641a9636f4c4efc4fab378188fba9d
JakevK <jakevankeulen@gmail.com>
2023-05-17 10:07:53 +1200

    Created GameLocation

---
+++ src/game/location/GameLocation.java
@@ -0,0 +1,34 @@
+package game.location;
+
+import game.GameEnvironment;
+
+/**
+ * Defines the skeleton of a GameLocation class that is
+ * implemented by each game location.
+ * 
+ * @author Jake van Keulen
+ * @version 1.0
+ *
+ */
+public abstract class GameLocation {
+	/**
+	 * The game environment object to which the location belongs
+	 */
+	private GameEnvironment gameEnvironment;
+	
+	/**
+	 * Get the game environment object to which the location belongs
+	 * @return		A GameEnvironment object.
+	 */
+	private GameEnvironment getGameEnvironment() {
+		return gameEnvironment;
+	}
+	
+	/**
+	 * Sets the current week of the season and updates the location's data accordingly.
+	 * This does different things depending on the location.
+	 * 
+	 * @param week		The new week of the season to update to.
+	 */
+	abstract public void update(int week);
+}
