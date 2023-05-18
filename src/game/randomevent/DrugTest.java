3c33d53e6a986e19c6c93f2b5de8b35411d3a52a
FindlayRoyds <findlayroyds@gmail.com>
2023-05-18 13:03:41 +1200

    created drug test random event

---
+++ src/game/randomevent/DrugTest.java
@@ -0,0 +1,79 @@
+package game.randomevent;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import game.GameEnvironment;
+import game.Team;
+import game.Athlete;
+import enumeration.Location;
+import enumeration.Position;
+
+/**
+ * A random drug test event that is triggered weekly.
+ * Every athlete in the player's team is tested for drug use.
+ * If any athletes are found to have used steroids, the game ends.
+ * 
+ * @author Findlay Royds
+ * @version 1.0, May 2023.
+ */
+public class DrugTest extends RandomEvent {
+
+	/**
+	 * The constructor for the drug test random event
+	 * 
+	 * @param gameEnvironment
+	 */
+	public DrugTest(GameEnvironment gameEnvironment) {
+		super(gameEnvironment);
+	}
+	
+	/**
+	 * Tests every athlete in the player's team for steroid use.
+	 * If any athletes have used steroids, the game location is changed to the end location
+	 */
+	@Override
+	protected void occur() {
+		boolean passedDrugTest = true;
+		final Team playerTeam = gameEnvironment.getPlayer().getTeam();
+		final HashMap<Position, Athlete> activeAthletes = playerTeam.getActiveAthletes();
+		final Set<Athlete> reserveAthletes = playerTeam.getReserveAthletes();
+		
+		// Test if any athletes have used steroids
+		for (Athlete athlete: activeAthletes.values()) {
+			if (athlete.getHasUsedSteroids()) {
+				passedDrugTest = false;
+				break;
+			}
+		}
+		for (Athlete athlete: reserveAthletes) {
+			if (athlete.getHasUsedSteroids()) {
+				passedDrugTest = false;
+				break;
+			}
+		}
+		
+		// If any athletes failed the drug test, end the game
+		if (!passedDrugTest) {
+			gameEnvironment.changeLocation(Location.END_SCREEN);
+			
+			final String popupMessage = "Your team failed a randomised drug test! You have been disqualified from the tournament.";
+			gameEnvironment.getUIEnvironment().displayPopup(popupMessage);
+		} else {
+			final String popupMessage = "Your team passed a randomised drug test.";
+			gameEnvironment.getUIEnvironment().displayPopup(popupMessage);
+		}
+	}
+	
+	/**
+	 * Calculate and return the probability of a randomised drug test occuring.
+	 * Probability is based on how far through the tournament you are in the game
+	 * 
+	 * @return								The probability of the event occuring in range: [0.0, 1.0)
+	 */
+	@Override
+	protected float getProbability() {
+		final float tournamentProgression = gameEnvironment.getWeek() / gameEnvironment.getSeasonLength();
+		return 0.1f * tournamentProgression;
+	}
+}
