cb1f8f4a5babbb66567aa83620f190fc44fd9b7b
FindlayRoyds <findlayroyds@gmail.com>
2023-05-19 21:40:47 +1200

    made a check for your team being eligible to play in a match

--- src/game/location/GameMatchSelection.java
+++ src/game/location/GameMatchSelection.java
@@ -1,16 +1,18 @@
 package game.location;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import enumeration.Location;
+import enumeration.Position;
+import game.Athlete;
 import game.GameEnvironment;
 import game.Team;
 
 /**
- * A class defining the MatchSelection location.
- * The MatchSelection location provides a list of randomly generated Teams that
- * the Player can choose to play against. This list is refreshed each week to
- * have a new set of Teams.
+ * A class defining the MatchSelection location. The MatchSelection location
+ * provides a list of randomly generated Teams that the Player can choose to
+ * play against. This list is refreshed each week to have a new set of Teams.
  * 
  * @author Jake van Keulen
  * @version 1.0
@@ -25,17 +27,17 @@ public class GameMatchSelection extends GameLocation {
 	 * The list of Teams that are available to play a match against.
 	 */
 	private ArrayList<Team> teams;
-	
+
 	/**
 	 * Constructor for Map
 	 */
 	public GameMatchSelection(GameEnvironment gameEnvironment) {
 		super(gameEnvironment);
 	}
-	
+
 	/**
-	 * Update the week to a given week.
-	 * This involves refreshing the opposition teams so that they are different each week.
+	 * Update the week to a given week. This involves refreshing the opposition
+	 * teams so that they are different each week.
 	 */
 	@Override
 	public void update(int week) {
@@ -47,19 +49,19 @@ public class GameMatchSelection extends GameLocation {
 	}
 
 	/**
-	 * @return		The teams that are available to play against.
+	 * @return The teams that are available to play against.
 	 */
 	public ArrayList<Team> getTeams() {
 		return teams;
 	}
-	
+
 	/**
-	 * Starts a match between the Player's team and the given opposingTeam.
-	 * Sets up the match by updating the Match location with the corresponding teams,
-	 * then changes the current location to the Match location.
-	 * This is all done through calling methods in the game environment.
+	 * Starts a match between the Player's team and the given opposingTeam. Sets up
+	 * the match by updating the Match location with the corresponding teams, then
+	 * changes the current location to the Match location. This is all done through
+	 * calling methods in the game environment.
 	 * 
-	 * @param opposingTeam		The team for the Player to play against.
+	 * @param opposingTeam The team for the Player to play against.
 	 */
 	public void playMatch(Team opposingTeam) {
 		GameMatch matchGameLocation = (GameMatch) getGameEnvironment().getGameLocation(Location.MATCH);
@@ -67,5 +69,27 @@ public class GameMatchSelection extends GameLocation {
 
 		matchGameLocation.setTeams(playerTeam, opposingTeam);
 		getGameEnvironment().changeLocation(Location.MATCH);
+	}
+
+	/**
+	 * Get whether a match is able to start. A match can only start when the
+	 * player's team has 5 active athletes,
+	 */
+	public boolean canStartMatch() {
+		Team playerTeam = getGameEnvironment().getPlayer().getTeam();
+		Map<Position, Athlete> activeAthletes = playerTeam.getActiveAthletes();
+		boolean canStart = false;
+
+		// Make sure that the player has a full team,
+		// and not all of the athletes are injured
+		for (Athlete athlete : activeAthletes.values()) {
+			if (athlete == null)
+				return false;
+			if (!athlete.isInjured()) {
+				canStart = true;
+				break;
+			}
+		}
+		return canStart;
 	}
 }
--- src/userinterface/commandline/CLIMatchSelection.java
+++ src/userinterface/commandline/CLIMatchSelection.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import enumeration.Location;
+import game.Team;
 import game.location.GameLocation;
 import game.location.GameMatchSelection;
-import game.Team;
-import enumeration.Location;
 
 /**
- * The command line match selection class.
- * Allows the user to view and select teams to play against.
+ * The command line match selection class. Allows the user to view and select
+ * teams to play against.
  * 
  * @author Findlay Royds
- * @version 1.0, May 2023.
+ * @version 1.2, May 2023.
  */
 public class CLIMatchSelection extends CLILocation {
 	/**
 	 * The game location this cli location is linked to.
 	 */
 	private GameMatchSelection gameLocation;
-	
+
 	/**
-	 * @param gameLocation					The game location this cli location is linked to.
+	 * @param gameLocation The game location this cli location is linked to.
 	 */
 	public CLIMatchSelection(GameLocation gameLocation, CLIEnvironment cliEnvironment) {
 		super(cliEnvironment);
@@ -33,22 +33,23 @@ public class CLIMatchSelection extends CLILocation {
 	@Override
 	public Location display() {
 		System.out.println("Stadium");
-		
+
 		ArrayList<Team> availableMatches = gameLocation.getTeams();
-		List<String> teamNames = availableMatches.stream()
-				.map(team -> team.getName())
-				.collect(Collectors.toList());
+		List<String> teamNames = availableMatches.stream().map(team -> team.getName()).collect(Collectors.toList());
 		teamNames.add("Exit to map");
 		int selectedOption = cliEnvironment.displayOptions(teamNames.toArray(new String[0]));
-		
+
 		// Test if user selected "Exit to map".
-		if (selectedOption == teamNames.size() - 1) {
+		if (selectedOption == teamNames.size() - 1)
 			return Location.MAP;
-		}
-		
-		// Start a match against the selected team.
-		gameLocation.playMatch(availableMatches.get(selectedOption));
-		
+
+		// Start a match against the selected team if the player's team is able to play
+		// a match
+		if (gameLocation.canStartMatch())
+			gameLocation.playMatch(availableMatches.get(selectedOption));
+		else
+			System.out.println("Your team is unable to play a match!");
+
 		// Return to map after the match.
 		return Location.MAP;
 	}
