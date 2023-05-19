e6de2bc343c6af7cc6c14872c314d7c1b03aa91c
FindlayRoyds <findlayroyds@gmail.com>
2023-05-19 13:49:25 +1200

    create cli match selection class

--- src/userinterface/commandline/CLIMatchSelection.java
+++ src/userinterface/commandline/CLIMatchSelection.java
@@ -1,6 +1,8 @@
 package userinterface.commandline;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import game.location.GameLocation;
 import game.location.GameMatchSelection;
@@ -31,10 +33,23 @@ public class CLIMatchSelection extends CLILocation {
 	@Override
 	public Location display() {
 		System.out.println("Stadium");
+		
 		ArrayList<Team> availableMatches = gameLocation.getTeams();
-		for (Team team : availableMatches) {
-			
+		List<String> teamNames = availableMatches.stream()
+				.map(team -> team.getName())
+				.collect(Collectors.toList());
+		teamNames.add("Exit to map");
+		int selectedOption = cliEnvironment.displayOptions(teamNames.toArray(new String[0]));
+		
+		// Test if user selected "Exit to map".
+		if (selectedOption == teamNames.size() - 1) {
+			return Location.MAP;
 		}
-		return null;
+		
+		// Start a match against the selected team.
+		gameLocation.playMatch(availableMatches.get(selectedOption));
+		
+		// Return to map after the match.
+		return Location.MAP;
 	}
 }
