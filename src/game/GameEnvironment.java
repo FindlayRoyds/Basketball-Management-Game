753ca8705418c06c42012c581c54d61273e1876b
JakevK <jakevankeulen@gmail.com>
2023-05-19 17:59:25 +1200

    Making progressWeek actually do something

--- src/game/GameEnvironment.java
+++ src/game/GameEnvironment.java
@@ -114,6 +114,8 @@ public class GameEnvironment {
 		));
 		
 		uiEnvironment = new CLIEnvironment(gameLocations, this);
+		currentWeek = 0;
+		progressWeek();
 	}
 
 	/**
@@ -223,6 +225,9 @@ public class GameEnvironment {
 	 */
 	public void progressWeek() {
 		currentWeek += 1;
+		for (GameLocation gameLocation: gameLocations.values()) {
+			gameLocation.update(currentWeek);
+		}
 		if (hasEnded()) {
 			changeLocation(Location.END);
 		}
