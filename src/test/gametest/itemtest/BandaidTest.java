b90961337ac3741a199898604673c5bc4b8b4465
FindlayRoyds <findlayroyds@gmail.com>
2023-05-22 11:10:52 +1200

    finished bandaid test

--- src/test/gametest/itemtest/BandaidTest.java
+++ src/test/gametest/itemtest/BandaidTest.java
@@ -1,6 +1,8 @@
 package test.gametest.itemtest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.swing.JFrame;
 
@@ -8,7 +10,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import enumeration.Position;
+import game.Athlete;
 import game.GameEnvironment;
+import game.item.Bandaid;
 import userinterface.graphical.GUIEnvironment;
 
 class BandaidTest {
@@ -28,8 +33,34 @@ class BandaidTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void constructorTest() {
+		Bandaid bandaid = new Bandaid("Test", 0, ge);
+		assertEquals("Test", bandaid.getDescription());
+		assertEquals(0, bandaid.getPrice());
+		bandaid = new Bandaid("", 100, ge);
+		assertEquals("", bandaid.getDescription());
+		assertEquals(100, bandaid.getPrice());
 	}
 
+	@Test
+	void generatorTest() {
+		Bandaid bandaid = (Bandaid) Bandaid.generateBandaid(100, ge);
+		assertTrue(bandaid instanceof Bandaid);
+	}
+
+	@Test
+	void applyTest() {
+		Athlete athlete = new Athlete("", Position.DUNKER, 0, ge, 0);
+		Bandaid bandaid = (Bandaid) Bandaid.generateBandaid(100, ge);
+		assertTrue(athlete.isInjured());
+		bandaid.applyItem(athlete);
+		assertFalse(athlete.isInjured());
+	}
+
+	@Test
+	void detailsTest() {
+		Bandaid bandaid = (Bandaid) Bandaid.generateBandaid(100, ge);
+		assertTrue(bandaid.getDetails() != null);
+		assertTrue(bandaid.getDetails() instanceof String);
+	}
 }
