285aa25ba4473cd64051d2a3235f37a1fd02b1a6
JakevK <jakevankeulen@gmail.com>
2023-05-22 11:22:57 +1200

    Wrote GameEnd unit tests and added to Athlete unit tests.

---
+++ src/test/gametest/locationtest/GameEndTest.java
@@ -0,0 +1,82 @@
+package test.gametest.locationtest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.swing.JFrame;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import enumeration.Location;
+import game.GameEnvironment;
+import game.Team;
+import game.location.GameEnd;
+import userinterface.graphical.GUIEnvironment;
+
+class GameEndTest {
+	private GameEnvironment ge;
+	private Team tm;
+	private GameEnd gameEnd;
+
+	@BeforeEach
+	void setUp() {
+		ge = new GameEnvironment(false);
+		ge.setSeed(0);
+		tm = ge.getPlayer().getTeam();
+		gameEnd = (GameEnd) ge.getGameLocation(Location.END);
+	}
+
+	@AfterEach
+	void tearDown() {
+		JFrame frame = ((GUIEnvironment) ge.getUIEnvironment()).getFrame();
+		frame.setVisible(false);
+		frame.dispose();
+	}
+
+	@Test
+	void updateTest() {
+		gameEnd.update(0);
+	}
+
+	@Test
+	void scoreTest() {
+		assertEquals(ge.getPlayer().getScore(), gameEnd.getScore());
+		ge.getPlayer().givePoints(123);
+		assertEquals(ge.getPlayer().getScore(), gameEnd.getScore());
+		ge.getPlayer().givePoints(2000);
+		assertEquals(ge.getPlayer().getScore(), gameEnd.getScore());
+	}
+
+	@Test
+	void moneyTest() {
+		assertEquals(ge.getPlayer().getMoney(), gameEnd.getMoney());
+		ge.getPlayer().giveMoney(100);
+		assertEquals(ge.getPlayer().getMoney(), gameEnd.getMoney());
+		ge.getPlayer().chargeMoney(100);
+		assertEquals(ge.getPlayer().getMoney(), gameEnd.getMoney());
+	}
+
+	@Test
+	void teamNameTest() {
+		ge.getPlayer().getTeam().setName("test name");
+		assertEquals(ge.getPlayer().getTeam().getName(), gameEnd.getTeamName());
+		ge.getPlayer().getTeam().setName("123");
+		assertEquals(ge.getPlayer().getTeam().getName(), gameEnd.getTeamName());
+	}
+
+	@Test
+	void seasonLengthTest() {
+		ge.setSeasonLength(5);
+		assertEquals(ge.getSeasonLength(), gameEnd.getSeasonLength());
+		ge.setSeasonLength(15);
+		assertEquals(ge.getSeasonLength(), gameEnd.getSeasonLength());
+	}
+
+	@Test
+	void weekTest() {
+		assertEquals(ge.getWeek(), gameEnd.getWeek());
+		ge.progressWeek();
+		assertEquals(ge.getWeek(), gameEnd.getWeek());
+	}
+}
